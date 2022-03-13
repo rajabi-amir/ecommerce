c3a40965d85e3af125991814b35e54fdc77e81a6
hoseinghsghs <44768710+hoseinghsghs@users.noreply.github.com>
2022-03-13 23:12:04 +0330

    date

--- app/Http/Controllers/Admin/ImageController.php
+++ app/Http/Controllers/Admin/ImageController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ImageController extends Controller
 {
     
-   public function UploadeBrandImage($request){
+   public function UploadeBrandImage($request ,$heigh , $width ,$path){
     if (isset($request->img)) {
         
     //درایور پیش فرض ذخیره   
@@ -24,14 +24,14 @@ class ImageController extends Controller
     //ساخت نام تصویر از هلپر فانکشن
     $image_name=Persian_generateImageName($extension);
     
-    $img=Image::make($request->img)->resize(50,50);
+    $img=Image::make($request->img)->resize($heigh,$width);
 
     if (!Storage::exists('brands')) {
         // این پوشه را بساز
         Storage::makeDirectory('brands');
     }
 
-    $img->save($pach . '/brands/' . $image_name);
+    $img->save($pach . '/' . $path . '/' . $image_name);
 
     return $image_name;
 }else{
