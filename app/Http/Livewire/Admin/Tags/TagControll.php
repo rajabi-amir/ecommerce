99d6476104801c5a0fea07b7e4b867a132f86917
hoseinghsghs <44768710+hoseinghsghs@users.noreply.github.com>
2022-04-04 14:44:45 +0430

    debug tag

--- app/Http/Livewire/Admin/Tags/TagControll.php
+++ app/Http/Livewire/Admin/Tags/TagControll.php
@@ -4,13 +4,25 @@ namespace App\Http\Livewire\Admin\Tags;
 
 use App\Models\Tag;
 use Livewire\Component;
+use Livewire\WithPagination;
+
 
 class TagControll extends Component
 {
+    use WithPagination;
     public $tag_name;
     public $tag;
     public $is_edit=false;
     public $tags;
+    public $display;
+
+    public function ref()
+    {
+        $this->is_edit=false;
+        $this->reset("tag_name");
+        $this->reset("display");
+    }
+
     
     public function render()
     {
@@ -18,40 +30,65 @@ class TagControll extends Component
         return view('livewire.admin.tags.tag-controll');
     }
 
-
-
+    
     public function edit_tag(Tag $tag){
 
         $this->is_edit=true;
         $this->tag_name=$tag->name;
         $this->tag=$tag;
+        $this->display="disabled";
               
     }
 
+   
     
     public function del_tag(Tag $tag){
-        $tag->delete();
+       
+        try {
+
+            $tag->delete();
+                      
+          } catch (\Exception $e) {
+         
+            redirect('admin.tags.create');
+         }
+        
+       
+
+        
     }
 
     public function addTag(){
-
+       
         if($this->is_edit){
-          
+
+          $this->validate([
+            'tag_name' => 'required|unique:tags,name'
+          ]);
+
         $this->tag->update([
         'name'=> $this->tag_name,
         ]);
         $this->is_edit=false;
-        $this->tag_name="";
+        $this->reset("tag_name");
+        $this->reset("display");
+       
+      
         }
         
         else{
+            $this->validate([
+                'tag_name' => 'required|unique:tags,name'
+              ]);
             Tag::create([
 
                 "name" => $this->tag_name,
                ]);
-               $this->tag_name="";
+               $this->reset("tag_name");
+             
 
         }
+       
       
     }
 
